0108cf7dad77f564b389ace8cd20c3fdd4f262a8
José Gama <jose.gama@ccc.ufcg.edu.br>
2025-06-18 13:54:40 -0300

    create main file

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-from llm_factory import LLMFactory
+from experiment_runner import ExperimentRunner
 
-# Para Hugging Face local
-config_hf = {
-    "provider": "huggingface",
-    "model_name": "mistralai/Mistral-7B-Instruct-v0.1",
-    "task": "text-generation",
-    "device": -1,
-    "token": ''
-}
-
-# Para OpenAI API
-config_openai = {
-    "provider": "openai",
-    "api_key": "",
-    "model_name": "gpt-4"
-}
-
-llm = LLMFactory.create_llm(config_hf)
-response = llm.generate("Explique o que é aprendizado de máquina.")
-print(response)
-
-llm_2 = LLMFactory.create_llm(config=config_openai)
-response_2 = llm_2.generate("Explique o que é aprendizado de máquina.")
-print(response_2)
+exp_runner = ExperimentRunner()
+exp_runner.execute()

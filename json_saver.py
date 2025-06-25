2cc002f01b308a994d2f4f23b984be87d8458c70
José Gama <jose.gama@ccc.ufcg.edu.br>
2025-06-25 09:40:38 -0300

    update json_saver method name

--- json_saver.py
+++ json_saver.py
@@ -3,14 +3,7 @@ import os
 
 class JSONSaver:
     @staticmethod
-    def save_results(data, filepath):
-        """
-        Salva os resultados no formato JSON no caminho especificado.
-
-        Args:
-            data (dict): Dados a serem salvos.
-            filepath (str): Caminho do arquivo JSON.
-        """
+    def save_json(data, filepath):
         os.makedirs(os.path.dirname(filepath), exist_ok=True)
         with open(filepath, "w", encoding="utf-8") as json_file:
             json.dump(data, json_file, indent=4, ensure_ascii=False)

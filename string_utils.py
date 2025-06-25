7ad6ec12ea8db31e5e9c2537f342247449556461
José Gama <jose.gama@ccc.ufcg.edu.br>
2025-06-25 15:32:15 -0300

    create a text normalizer method

---
+++ string_utils.py
@@ -0,0 +1,15 @@
+import re
+
+class StringUtils:
+
+    @staticmethod
+    def normalize_text(text):
+        if not isinstance(text, str):
+            return ''
+
+        text = text.lower()
+        text = re.sub(r'[^\w\s]', '', text, flags=re.UNICODE)
+        text = re.sub(r'_', '', text)
+        text = re.sub(r'\s+', ' ', text).strip()
+
+        return text

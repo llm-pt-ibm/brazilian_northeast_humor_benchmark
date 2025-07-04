5ffbe035f5138e7e3407c8ce50a9498c6b6f07f5
José Gama <jose.gama@ccc.ufcg.edu.br>
2025-07-04 11:54:55 -0300

    create a flow for ibmcloud

--- llm_factory.py
+++ llm_factory.py
@@ -1,6 +1,7 @@
 from hugging_face_llm import HuggingFaceLLM
 from open_ai_llm import OpenAiLLM
 from gemini_llm import GeminiLLM
+from ibm_cloud_llm import IBMCloudLLM
 
 class LLMFactory:
     @staticmethod
@@ -17,13 +18,21 @@ class LLMFactory:
         elif provider == "openai":
             return OpenAiLLM(
                 api_key=config["api_key"],
-                model_name=config.get("model_name", "gpt-4")
+                model_name=config.get("model_name")
             )
 
         elif provider == "gemini":
             return GeminiLLM(
                 api_key=config["api_key"],
-                model_name=config.get("model_name", "gemini-pro")
+                model_name=config.get("model_name")
+            )
+
+        elif provider == 'ibmcloud':
+            return IBMCloudLLM(
+                model_id=config.get("model_id"),
+                api_key=config["api_key"],
+                service_url=config["service_url"],
+                project_id=config["project_id"]
             )
 
         else:

01862af1c79a5ed8f3ff01a3907c57ccb0734e25
José Gama <jose.gama@ccc.ufcg.edu.br>
2025-08-07 11:04:47 -0300

    update model name

--- hugging_face_llm.py
+++ hugging_face_llm.py
@@ -4,7 +4,7 @@ class HuggingFaceLLM:
     def __init__(self, model_name: str, task: str = "text-generation", device: int = -1, token: str = None):
         tokenizer = AutoTokenizer.from_pretrained(model_name, token=token)
         model = AutoModelForCausalLM.from_pretrained(model_name, token=token)
-        self.model_name = model_name
+        self.model_name = model_name.split('/')[1] if '/' in model_name else model_name
 
         self.pipeline = pipeline(
             task,

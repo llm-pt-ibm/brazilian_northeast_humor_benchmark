e336288b80b6d427daaa7c85344788c962b1a23c
José Gama <jose.gama@ccc.ufcg.edu.br>
2025-08-20 11:12:44 -0300

    include a parameter for few shot (uncompleted)

--- llm_prompt_manager.py
+++ llm_prompt_manager.py
@@ -4,7 +4,7 @@ class LLMPromptManager:
     def __init__(self):
         self.comic_styles_manager = ComicStylesManager()
 
-    def get_punchlines_prompt(self, humorous_text):
+    def get_punchlines_prompt(self, humorous_text, include_examples = False):
         punchlines_prompt = f'''Dado o seguinte texto humorístico, identifique todas as punchlines presentes.
 Definição de punchline: A punchline é a parte de uma piada que provoca o efeito cômico, sendo responsável pela resolução da piada. Ela ocorre quando o receptor reinterpreta a informação e faz uma conexão inesperada entre as partes do texto, gerando o riso.
 Identifique apenas as partes que representam a resolução cômica (punchlines).
@@ -19,7 +19,7 @@ Responda apenas no formato de lista.'''
         
         return punchlines_prompt
 
-    def get_comic_styles_prompts(self, humorous_text):
+    def get_comic_styles_prompts(self, humorous_text, include_examples = False):
         styles_definitions = self.comic_styles_manager.get_styles_definitions()
         comic_styles_prompts = {
         comic_style:f'''Dado o seguinte texto humorístico, avalie se ele contém o estilo cômico ”{self.comic_styles_manager.get_comic_style_pt_br_translation(comic_style)}”.
@@ -32,7 +32,7 @@ Não inclua explicações ou qualquer outro texto além do número.'''
 
         return comic_styles_prompts
     
-    def get_text_explanation_prompt(self, humorous_text):
+    def get_text_explanation_prompt(self, humorous_text, include_examples = False):
         text_explanation_prompt = f'''Explique o motivo do humor presente no seguinte texto. Aponte os elementos que contribuem para seu efeito cômico.
 Texto humorístico: {humorous_text}
 Responda apenas com a explicação, sem detalhes adicionais.'''

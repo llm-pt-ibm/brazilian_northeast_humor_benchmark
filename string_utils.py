142f0f8d7377496be40b3a9c3c98a3e9851042d7
José Gama <jose.gama@ccc.ufcg.edu.br>
2025-09-22 14:23:48 -0300

    add str2bool method

--- string_utils.py
+++ string_utils.py
@@ -1,4 +1,5 @@
 import re
+import argparse
 import ast
 
 class StringUtils:
@@ -101,3 +102,14 @@ class StringUtils:
         text = re.sub(r"\['([\"'])(.*?)\1'\]", r"['\2']", text)
 
         return text
+    
+    @staticmethod
+    def str2bool(v):
+        if isinstance(v, bool):
+            return v
+        if v.lower() in ("yes", "true", "t", "1"):
+            return True
+        elif v.lower() in ("no", "false", "f", "0"):
+            return False
+        else:
+            raise argparse.ArgumentTypeError("Boolean value expected (true/false).")
